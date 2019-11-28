aa7ccd8f09bee8df452e6d07cf6c1e27ca451649
stucork@gmail.com <stucork@gmail.com>
2019-11-28 14:36:56 +0000

    Saved automatically

--- modules/popover.py
+++ modules/popover.py
@@ -13,23 +13,26 @@ def popover(self, content,
             animation=True, 
             delay={ "show": 100, "hide": 100 }
            ):
-  """self - the element you wish to have the popover
+  """should be called by a button or link
   content - either text or an anvil component or Form
-  placement -  right, left, top, bottom
+  placement -  right, left, top, bottom (for left/right best to have links and buttons inside flow panels)
   trigger - manual, focus, hover, click
   animation - True or False
   delay - {'show': 100, 'hide': 100}
+  
+  if the content is a form then the form will have an attribute self.popper added
   """
   if isinstance(content, str):
     html = False
   else:
     html = True
+    # add the popper to the content form
     content.popper = self
 
   js.call_js('popover', self, content, title, placement, trigger, animation, delay, html)
 
 def pop(self,behaviour):
-  """behavior can either be any of
+  """behaviour can be any of
   show, hide, toggle, dispose, enable, disable, toggleEnabled, update"""
   js.call_js('pop', self, behaviour)
   
